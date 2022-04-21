97da49e405768e74327c4b32f7c47dec819f5ee5
Salma Oumoussa <85805792+SalmaOumoussa@users.noreply.github.com>
2022-04-21 10:49:19 +0000

    User Model

--- leads/models.py
+++ leads/models.py
@@ -1,11 +1,13 @@
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    pass
 
 class Agent(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+  
     
 
 class Lead(models.Model):
