9d97bf32ee00dc5432a4fc804dec52db1fda4649
Salma Oumoussa <85805792+SalmaOumoussa@users.noreply.github.com>
2022-05-23 13:37:35 +0100

    Unassigned leads & Assign agent

--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Lead
+from .models import Lead,Agent
 from django.contrib.auth.forms import UserCreationForm,UsernameField
 from django.contrib.auth import  get_user_model
 
@@ -23,9 +23,17 @@ class LeadModelForm(forms.ModelForm):
             'agent',
             )
 
-
 class LeadForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
-    age = forms.IntegerField(min_value=0)
+    age = forms.IntegerField(min_value=18)
     
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop("request")
+        agents = Agent.objects.filter(organization=request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs)
+        self.fields["agent"].queryset = agents
+
--- leads/views.py
+++ leads/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render,redirect,reverse
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm,LeadModelForm,CustomUserCreationForm
+from .forms import LeadForm,LeadModelForm,CustomUserCreationForm,AssignAgentForm
 from django.views import generic 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from agents.mixins import OrganisorAndLoginRequiredMixin
@@ -39,6 +39,19 @@ class LeadListView(LoginRequiredMixin,generic.ListView):
         # filtrer  for the agentlogged in 
             queryset = queryset.filter(agent__user=user)
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super(LeadListView, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_organisor:
+            queryset = Lead.objects.filter(
+                organization=user.userprofile, 
+                agent__isnull=True
+            )
+            context.update({
+                "unassigned_leads": queryset
+            }) 
+        return context    
 
 #Get all Leads based on function 
 def lead_list(request):
@@ -164,6 +177,29 @@ def lead_create(request):
 
 #     }
 #     return render(request,"leads/lead_update.html",context)
+
+
+
+class AssignAgentView(OrganisorAndLoginRequiredMixin, generic.FormView):
+    template_name = "leads/assign_agent.html"
+    form_class = AssignAgentForm
+
+    def get_form_kwargs(self, **kwargs):
+        kwargs = super(AssignAgentView, self).get_form_kwargs(**kwargs)
+        kwargs.update({
+            "request": self.request
+        })
+        return kwargs
+        
+    def get_success_url(self):
+        return reverse("leads:lead_list")
+
+    def form_valid(self, form):
+        agent = form.cleaned_data["agent"]
+        lead = Lead.objects.get(id=self.kwargs["pk"])
+        lead.agent = agent
+        lead.save()
+        return super(AssignAgentView, self).form_valid(form)
 
 def lead_update(request,pk):
     lead= Lead.objects.get(id=pk)
